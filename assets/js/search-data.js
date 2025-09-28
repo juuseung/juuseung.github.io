dd4f5d472a54af2282c85e3296388d44be1db997
juuseung <41817239+juuseung@users.noreply.github.com>
2025-09-28 20:46:35 +0000

    Deploying to gh-pages from @ juuseung/juuseung.github.io@eb2ed6c6e5fcb7dc5dc337d0279dc8b624197e94 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "teaching",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
